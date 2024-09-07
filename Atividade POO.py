0850f280e745c66aed3fc98860f9bccb5121d531
Carlos Eduardo <eduardocstsouza@gmail.com>
2024-09-07 15:10:03 -0300

    Update and rename main.py to Atividade POO.py

--- Atividade POO.py
+++ Atividade POO.py
@@ -1,3 +1,4 @@
+#QUESTAO 1
 class Livro:
     def __init__(self, titulo, autor, ano_publicacao):
         self.titulo = titulo
@@ -13,6 +14,7 @@ print(livro1.titulo, livro1.autor,  livro1.ano_publicacao)
 print(livro2.titulo, livro2.autor,  livro2.ano_publicacao)
 print(livro3.titulo, livro3.autor,  livro3.ano_publicacao)
 
+#QUEESTAO 2
 class produto:
     def __init__(self, *args, **kwargs):
         if len(args) >= 1:
@@ -34,5 +36,36 @@ produto2 = produto ("Tablet")
 
 print(produto1)
 print(produto2)
+
+
+#QUESTAO 3
+class contaBancaria:
+    def __init__(self, numero, titular, saldo):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar (self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print ('Saldo insuficiente')
+    def mostrarSaldo(self):
+        print(f"Seu saldo atual é {self.saldo}")
+
+
+conta1 = contaBancaria (123,'Eduardo', 5000)
+conta1.depositar (200)
+conta1.mostrarSaldo()
+conta1.sacar (500)
+
+conta2 = conta1
+conta2.depositar(100)
+conta2.mostrarSaldo()
+
+print (conta1 is conta2)
 
         
